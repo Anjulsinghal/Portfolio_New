7d94b1ad0e3817e2aa3195f3bfbec1b85c6f5dbc
Anjulsinghal <95030018+Anjulsinghal@users.noreply.github.com>
2024-07-29 00:51:58 +0530

    Update page.tsx
    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Intro />
       <div className="flex flex-col items-center px-4">
+        <Analytics />
         <SectionDivider />
         <Projects />
         <Skills />
