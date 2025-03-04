e7ec749f5e6dbf6bc005dc314244a51c2eab9843
Anjulsinghal <anjulsinghal123@gmail.com>
2025-03-05 01:47:30 +0530

    Add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "@/components/header";
 import { Nunito } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
@@ -20,6 +21,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <Head>
+        {/* <title>Anjul Singhal | Software Engineer & Web Developer</title> */}
+        <meta name="description" content="Anjul Singhal is a software engineer specializing in web development, AI integration, and scalable applications using Next.js and MERN stack." />
+        <meta name="keywords" content="Anjul Singhal, Web Developer, Software Engineer, Next.js, MERN Stack, AI, JavaScript, React, Portfolio" />
+        <meta name="author" content="Anjul Singhal" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Anjul Singhal | Software Engineer & Web Developer" />
+        <meta property="og:description" content="Explore Anjul Singhal's portfolio showcasing AI-driven projects, Next.js applications, and innovative software solutions." />
+        <meta property="og:url" content="https://anjul.live" />
+        <meta property="og:image" content="https://drive.google.com/file/d/1Tfz_9a49rz0YtHSSvqwXSamPM73rzQtP/view?usp=drive_link" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Anjul Singhal | Software Engineer & Web Developer" />
+        <meta name="twitter:description" content="Explore Anjul Singhal's portfolio showcasing AI-driven projects, Next.js applications, and innovative software solutions." />
+        <meta name="twitter:image" content="https://drive.google.com/file/d/1Tfz_9a49rz0YtHSSvqwXSamPM73rzQtP/view?usp=drive_link" />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://anjul.live" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <body
         className={`${nunito.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
@@ -27,7 +55,6 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
